e6a73add9142ed6a92fea211c0499abc87c6b781
alexc <alexandre.camus@edu.ece.fr>
2023-11-09 11:52:57 +0100

    Test

--- snoopy/main.c
+++ snoopy/main.c
@@ -42,7 +42,7 @@ int main() {
     }
     affichage();
 
-printf("quelle est la direction ekeh ?\n");
+printf("quelle est la direction?\n");
 scanf("%d", direction);
     switch (direction) {
         case 'Q':
